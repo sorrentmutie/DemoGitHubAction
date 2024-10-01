25e2b488caa2474d735ae1112de0a3d4515b50a0
salvatore sorrentino <salvatore.sorrentino@live.com>
2024-10-01 10:11:03 +0200

    Aggiungere i file di progetto.

---
+++ WebApplication1/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+           // throw new Exception("This is an exception");
+        }
+    }
+
+}
